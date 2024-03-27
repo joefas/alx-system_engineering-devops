cf18a04ef64ae63a399fb2fdc71786ea55a3ee8b
joefas <ochienggeofrey06@gmail.com>
2024-03-27 06:01:39 -0700

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create_school_file.pp
 
 file { '/tmp/school':
-ensure => 'file',
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
-content => 'I love Puppet',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
